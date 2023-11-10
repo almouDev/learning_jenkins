2f64746456814d1ac7862e0141f2ee5564d7dbc5
Abdourahamane Issoufou Al Moustapha <issoufoos1234@gmail.com>
2023-11-10 18:40:00 +0100

    editing the jenkinsfiles accordingly to the modification made to the agent

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-def hello():
-	print("Good morning everybody from almoustapha, a nice guy")
+def tri_par_selection(A):
+    n = len(A)
+    B=[]
+    for i in range(n - 1):
+        indice_min = i
+        for j in range(i + 1, n):
+            if A[j] < A[indice_min]:
+                indice_min = j
+        B.append(A[indice_min])
+        A[i], A[indice_min] = A[indice_min], A[i]
+    return B
 
-hello()
+# Exemple d'utilisation
+A = [64, 25, 12, 22, 11]
+tri_par_selection(A)
+print("Tableau trié par sélection : ", A)
